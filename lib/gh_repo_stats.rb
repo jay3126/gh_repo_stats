720d921b8f28fa818fcffbb93c84e6fd14c364ce
jesalg <jesal7@gmail.com>
2014-01-15 16:07:25 -0800

    Setting up Thor for command line interaction

--- lib/gh_repo_stats.rb
+++ lib/gh_repo_stats.rb
@@ -1,5 +1,25 @@
-require "gh_repo_stats/version"
+require "thor"
+
+ROOT_DIR = File.dirname(__FILE__)
+
+require File.join(ROOT_DIR, "gh_repo_stats/version")
+require File.join(ROOT_DIR, "gh_repo_stats/query")
 
 module GhRepoStats
-  # Your code goes here...
+  class CLI < Thor
+    default_task :fetch
+
+    desc "[--after DATETIME] [--before DATETIME] [--event EVENT_NAME] [-count COUNT]", "Fetch the most active repositories for a given event & time range on GitHub"
+    option :after, banner: "Show activity from this DATETIME", required: true
+    option :before, banner: "Show activity to this DATETIME", required: true
+    option :event, banner: "Filter by EVENT_NAME", required: true
+    option :count, banner: "Limit the number of results to this COUNT (default 10)", default: 10, type: :numeric
+
+    def fetch
+      grs = GhRepoStats::Query.new(options)
+      grs.execute
+      puts grs.to_s
+    end
+
+  end
 end
