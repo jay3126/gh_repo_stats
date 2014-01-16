48c06bcf4c4b768ea72e7ec2e81d79bf569c15da
jesalg <jesal7@gmail.com>
2014-01-15 16:08:14 -0800

    Adding core implementation

---
+++ lib/gh_repo_stats/query.rb
@@ -0,0 +1,74 @@
+require 'open-uri'
+require 'zlib'
+require 'yajl'
+require 'ruby-progressbar'
+
+module GhRepoStats
+  class Query
+    attr_accessor :after, :before, :event_type, :count
+    attr_reader :events, :repos, :progress
+
+    def initialize(options={})
+      @events = []
+      @repos = []
+
+      @after  = DateTime.parse(options[:after])
+      @before = DateTime.parse(options[:before])
+
+      @event_type  = options[:event]
+      @count  = options[:count]
+    end
+
+    def execute
+      threads = []
+
+      @after.step(@before, 1.0/24) do |date|
+        threads << Thread.new do
+          filename = date.strftime("%Y-%m-%d-%k").gsub(/\s+/, "")
+          url = "http://data.githubarchive.org/#{filename}.json.gz"
+          parse(url)
+        end
+      end
+
+      @progress = ProgressBar.create(title: "Fetching Data:", total: threads.count, format: '%t |%b>>%i| %p% Complete')
+
+      threads.each { |thread|  thread.join }
+
+      aggregate
+    end
+
+    def to_s
+      output = []
+      @repos.each do |repo, entries|
+        output << "#{repo.gsub('https://github.com/','')} - #{entries.length} events"
+      end
+      output.join("\n")
+    end
+
+    private
+
+    def parse(url)
+      js = Zlib::GzipReader.new(open(url)).read
+
+      Yajl::Parser.parse(js) do |event|
+        event['slug'] = event['repository'].nil? ? event['url'] : event['repository']['url']
+        @events << event if event_date_within_range?(DateTime.parse(event['created_at'])) && event_type_match?(event['type'])
+      end
+
+      @progress.increment
+    end
+
+    def aggregate
+      @repos = @events.group_by { |event| event['slug'] }.sort_by { |slug, events| events.length }.reverse.slice(0..(@count.to_i - 1))
+    end
+
+    def event_date_within_range?(event_date)
+      event_date > @after && event_date < @before
+    end
+
+    def event_type_match?(event_type)
+      @event_type == event_type
+    end
+
+  end
+end
